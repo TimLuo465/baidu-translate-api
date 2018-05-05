95c3cce14efd68f05be20dd5667ac07775c650eb
Tim Luo <timluo465@gmail.com>
2018-05-05 22:23:44 +0800

    init

---
+++ src/translate.js
@@ -0,0 +1,22 @@
+const translate = {
+    google: {
+        url: `https://translate.google.com/translate_a/single?`,
+        params: {
+            client: 't',
+            sl: opts.from,
+            tl: opts.to,
+            hl: opts.to,
+            dt: ['at', 'bd', 'ex', 'ld', 'md', 'qca', 'rw', 'rm', 'ss', 't'],
+            ie: 'UTF-8',
+            oe: 'UTF-8',
+            otf: 1,
+            ssel: 0,
+            tsel: 0,
+            kc: 7,
+        }
+    }
+}
+
+export default (opts) => {
+    
+}
