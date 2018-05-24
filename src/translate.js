c7a0a52851b159bb115e4fcc0067121568f74136
Tim Luo <timluo465@gmail.com>
2018-05-24 23:19:15 +0800

    init

--- src/translate.js
+++ src/translate.js
@@ -1,22 +1,31 @@
+import axios from "axios";
+
 const translate = {
     google: {
         url: `https://translate.google.com/translate_a/single?`,
         params: {
             client: 't',
-            sl: opts.from,
-            tl: opts.to,
+            tl: opts.from,
+            sl: opts.to,
             hl: opts.to,
             dt: ['at', 'bd', 'ex', 'ld', 'md', 'qca', 'rw', 'rm', 'ss', 't'],
             ie: 'UTF-8',
             oe: 'UTF-8',
             otf: 1,
-            ssel: 0,
+            ssel: 3,
             tsel: 0,
             kc: 7,
+            tk: 796902.676339,
+            q: opts.query
+        },
+        getToken: function() {
+            var params = this.params;
+
+            
         }
     }
-}
+};
 
 export default (opts) => {
-    
+    axios.get()
 }
