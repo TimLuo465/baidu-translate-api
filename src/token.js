8bf34f094d3db3d5e566c627762ba45f1dc0d3ee
timluo465 <1664311406@qq.com>
2018-05-28 20:16:47 +0800

    init

--- src/token.js
+++ src/token.js
@@ -1,58 +1,90 @@
-/** 
- * https://translate.google.com/translate/releases/twsfe_w_20180521_RC03/r/js/desktop_module_main.js 
+/**
+ * http://fanyi.bdstatic.com/static/translation/pkg/index_498ea34.js
+ * 
+ * Everything between 'BEGIN' and 'END' was copied from the url above.
  */
-let tr = null;
-const window = {
-    TKK: eval('((function(){var a\x3d338222765;var b\x3d924334499;return 424232+\x27.\x27+(a+b)})())')
-};
-const rr = a => () => a;
-const sr = function (a, b) {
-    for (var c = 0; c < b.length - 2; c += 3) {
-        var d = b.charAt(c + 2);
-        d = "a" <= d ? d.charCodeAt(0) - 87 : Number(d);
-        d = "+" == b.charAt(c + 1) ? a >>> d : a << d;
-        a = "+" == b.charAt(c) ? a + d & 4294967295 : a ^ d
-    }
-    return a;
-};
-const ur = a => {
-    if (null !== tr)
-        var b = tr;
-    else {
-        b = rr(String.fromCharCode(84));
-        var c = rr(String.fromCharCode(75));
-        b = [b(), b()];
-        b[1] = c();
-        b = (tr = window[b.join(c())] || "") || ""
-    }
-    var d = rr(String.fromCharCode(116));
-    c = rr(String.fromCharCode(107));
-    d = [d(), d()];
-    d[1] = c();
-    c = "&" + d.join("") + "=";
-    d = b.split(".");
-    b = Number(d[0]) || 0;
-    for (var e = [], f = 0, g = 0; g < a.length; g++) {
-        var l = a.charCodeAt(g);
-        128 > l ? e[f++] = l : (2048 > l ? e[f++] = l >> 6 | 192 : (55296 == (l & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (l = 65536 + ((l & 1023) << 10) + (a.charCodeAt(++g) & 1023),
-            e[f++] = l >> 18 | 240,
-            e[f++] = l >> 12 & 63 | 128) : e[f++] = l >> 12 | 224,
-            e[f++] = l >> 6 & 63 | 128),
-            e[f++] = l & 63 | 128)
-    }
-    a = b;
-    for (f = 0; f < e.length; f++)
-        a += e[f],
-            a = sr(a, "+-a^+6");
-    a = sr(a, "+-3^+b+-f");
-    a ^= Number(d[1]) || 0;
-    0 > a && (a = (a & 2147483647) + 2147483648);
-    a %= 1E6;
-    return c + (a.toString() + "." + (a ^ b))
-};
 
-module.exports = {
-    get: () => {
 
+// BEGIN
+
+var i = null;
+
+function a(r) {
+    if (Array.isArray(r)) {
+        for (var o = 0, t = Array(r.length); o < r.length; o++)
+            t[o] = r[o];
+        return t
     }
+    return Array.from(r)
 }
+
+function n(r, o) {
+    for (var t = 0; t < o.length - 2; t += 3) {
+        var a = o.charAt(t + 2);
+        a = a >= "a" ? a.charCodeAt(0) - 87 : Number(a),
+            a = "+" === o.charAt(t + 1) ? r >>> a : r << a,
+            r = "+" === o.charAt(t) ? r + a & 4294967295 : r ^ a
+    }
+    return r
+}
+
+function e(r) {
+    var o = r.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g);
+    if (null === o) {
+        var t = r.length;
+        t > 30 && (r = "" + r.substr(0, 10) + r.substr(Math.floor(t / 2) - 5, 10) + r.substr(-10, 10))
+    } else {
+        for (var e = r.split(/[\uD800-\uDBFF][\uDC00-\uDFFF]/), C = 0, h = e.length, f = []; h > C; C++)
+            "" !== e[C] && f.push.apply(f, a(e[C].split(""))),
+                C !== h - 1 && f.push(o[C]);
+        var g = f.length;
+        g > 30 && (r = f.slice(0, 10).join("") + f.slice(Math.floor(g / 2) - 5, Math.floor(g / 2) + 5).join("") + f.slice(-10).join(""))
+    }
+    var u = void 0
+        , l = "" + String.fromCharCode(103) + String.fromCharCode(116) + String.fromCharCode(107);
+    u = null !== i ? i : (i = window[l] || "") || "";
+    for (var d = u.split("."), m = Number(d[0]) || 0, s = Number(d[1]) || 0, S = [], c = 0, v = 0; v < r.length; v++) {
+        var A = r.charCodeAt(v);
+        128 > A ? S[c++] = A : (2048 > A ? S[c++] = A >> 6 | 192 : (55296 === (64512 & A) && v + 1 < r.length && 56320 === (64512 & r.charCodeAt(v + 1)) ? (A = 65536 + ((1023 & A) << 10) + (1023 & r.charCodeAt(++v)),
+            S[c++] = A >> 18 | 240,
+            S[c++] = A >> 12 & 63 | 128) : S[c++] = A >> 12 | 224,
+            S[c++] = A >> 6 & 63 | 128),
+            S[c++] = 63 & A | 128)
+    }
+    for (var p = m, F = "" + String.fromCharCode(43) + String.fromCharCode(45) + String.fromCharCode(97) + ("" + String.fromCharCode(94) + String.fromCharCode(43) + String.fromCharCode(54)), D = "" + String.fromCharCode(43) + String.fromCharCode(45) + String.fromCharCode(51) + ("" + String.fromCharCode(94) + String.fromCharCode(43) + String.fromCharCode(98)) + ("" + String.fromCharCode(43) + String.fromCharCode(45) + String.fromCharCode(102)), b = 0; b < S.length; b++)
+        p += S[b],
+            p = n(p, F);
+    return p = n(p, D),
+        p ^= s,
+        0 > p && (p = (2147483647 & p) + 2147483648),
+        p %= 1e6,
+        p.toString() + "." + (p ^ m)
+}
+
+// END
+
+const request = require("request");
+
+// 4768850
+function updateGTK() {
+    return new Promise((resolve, reject) => {
+        request.get("http://fanyi.baidu.com", (err, res, body) => {
+            const regExp = /gtk\s=\s'(.*?)';/g;
+            let code = body.match(regExp);
+
+            if (code) {
+                i = code[0].replace(regExp, "$1");
+            }
+            resolve();
+        });
+    });
+}
+
+
+module.exports.get = text => {
+    return updateGTK().then(() => {
+        let sign = e(text);
+
+        return { sign };
+    });
+};
--- src/translate.js
+++ src/translate.js
@@ -1,45 +1,15 @@
-const axios = require("axios");
+const request = require("request");
 
-const translate = {
-    google: {
-        url: `https://translate.google.com/translate_a/single`,
-        getToken: function(opts) {
-            let params = {
-                client: 't',
-                tl: opts.from,
-                sl: opts.to,
-                hl: opts.to,
-                dt: ['at', 'bd', 'ex', 'ld', 'md', 'qca', 'rw', 'rm', 'ss', 't'],
-                ie: 'UTF-8',
-                oe: 'UTF-8',
-                otf: 1,
-                ssel: 0,
-                tsel: 0,
-                kc: 1,
-                tk: 796902.676339,
-                q: opts.query
-            };
-            let tokens = Object.keys(params);
-            let len = tokens.length;
+// BAIDUID 396DB40732A5AFFEE75D334BF3122085:FG=1
+const jar = request.jar();
+const cookies = request.cookie("BAIDUID=FE4877CC01DBC1F78D71472CE16CDAB2:FG=1")
+const url = "http://fanyi.baidu.com/v2transapi?from=zh&to=en&query=%E6%B5%8B%E8%AF%95&transtype=realtime&simple_means_flag=3&sign=706553.926920&token=6084690412299b5fcf05ecb68da2e9a1";
 
-            return tokens.reduce((prev, key, i) => {
-                let param = `${key}=${params[key]}${len - 1 > i ? "&" : ""}`;
+jar.setCookie(cookies, url)
 
-                if(key === "dt") {
-                    param = params.dt.reduce((prev, k) => prev + `dt=${k}&` , "");
-                }
-
-                return prev + param;
-            }, "");
-        }
-    }
-};
-
-module.exports = (opts) => {
-    let g = translate.google;
-    let url = `${g.url}?${g.getToken(opts)}`;
-    console.log(url)
-    axios.get(url).then(res => {
-        console.log(res);
-    });
-}
+request({
+    url: url,
+    jar: jar
+}, (err, res, body) => {
+    console.log(JSON.parse(decodeURIComponent(body)));
+});
